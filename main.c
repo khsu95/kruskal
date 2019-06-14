7e79b5edc555e85554b0b7c27a1cbc21bf1d762e
HS.K <khsu95@gmail.com>
2019-06-15 02:16:47 +0900

    Main.c modifyied

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<conio.h>
 #include"main.h"
 #include"Min_Heap.h"
 #include"set.h"
@@ -20,15 +21,15 @@ int main()
 	while (1)
 	{
 		printf("Edge: ");
-		ret = fscanf(inout, "%c %c %d", &temp.v, &temp.v, &temp.key);
+		ret = fscanf(inout, "%c %c %d\n", &temp.v, &temp.w, &temp.key);
 		if (ret == EOF)		break; /*파일의 끝에 도착하면 EOF*/
 		printf("\n");
 		if (!('A' <= temp.v <= 'Z') || !('A' <= temp.w <= 'Z') || temp.key < 0) {
 			printf("Wrong Input===========Node is Alphabet, Weight isn't minus \n");
 			break;
 		}
-		push(&HEAP_INDEX, temp);/*간선과 노드 추가*/
-		getchar();/*버퍼에 남은 데이터 초기화*/
+ 		push(&HEAP_INDEX, temp);/*간선과 노드 추가*/
+		//if(HEAP_INDEX>2) getchar();/*버퍼에 남은 데이터 초기화*/
 		if (temp.v > big || temp.w > big)
 		{
 			if (temp.v > temp.w)	big = temp.v;
@@ -36,23 +37,23 @@ int main()
 		}
 	} //INPUT
 	fclose(inout);
+	for (int i = 97; i <= 122; i++)/*정점의 개수만큼 Set을 만든다.*/
+		set[i] = i;
 
-	for (int i = 0; i <= big - 64; i++)/*정점의 개수만큼 Set을 만든다.*/
-		set[i] = 64 + i;
-
-	num_edge = HEAP_INDEX;
-	selected_edge = 1;
+	num_edge = big - 'a' + 1;
+	selected_edge = 0;
 
 	while (selected_edge < num_edge)
 	{
-		if (num_edge == -1)
+		if (num_edge == -1||(selected_edge==num_edge-1))
 		{
-			printf("EMPTY TREE \n");
+			printf("EMPTY TREE or Comlete MST \n");
 			break;
 		}
-
 		temp = pop(&HEAP_INDEX);/*정렬된 MinHeap에서 최소 간선 꺼냄*/
-
+		if (temp.v < 97 || temp.w < 97 || temp.key < 0) {
+			printf("POP ERROR\n");	break;
+		}
 
 		V_R = Find(temp.v);/*v정점의 집합을 찾음*/
 		W_R = Find(temp.w);/*w정점의 집합을 찾음*/
@@ -60,7 +61,7 @@ int main()
 		if (V_R.vertical != W_R.vertical)/*동성동본이 아니라면*/
 		{
 			Union(V_R, W_R);/*백년가약*/
-			MST[selected_edge] = temp;/*선택 받은 i번째 간선*/
+			MST[selected_edge+1] = temp;/*선택 받은 i번째 간선*/
 			selected_edge++;
 		}
 	}
@@ -69,12 +70,13 @@ int main()
 		printf("No spaaning Tree \n");
 		return 0;
 	}
-	inout = fopen("in.txt", "wt");/*바이너리 형식으로 저장할 것임*/
+	inout = fopen("out.txt", "wt");/*바이너리 형식으로 저장할 것임*/
 	for (int i = 1; i <= selected_edge; i++)
 		//fread((void*)MST, sizeof(*MST), 1, inout);
 		fprintf(inout,"%c---%c = %d\n", MST[i].v, MST[i].w, MST[i].key);
 	fputs("End of Programm \n",inout);
 	
 	fclose(inout);
+	_getch();
 	return 0;
 }
